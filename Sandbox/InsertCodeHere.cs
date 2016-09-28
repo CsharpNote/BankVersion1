24d6600365ecfffd9d65e530a026f82c610f7c03
EASJ <EASJ@EASJ3212>
2016-09-28 20:20:08 +0200

    Add project files.

---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
