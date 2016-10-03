19172b4a51cbddc564a6f797673eed5bcfd57209
Martin Kierkegaard <mark@easj.dk>
2016-10-03 20:10:28 +0200

    Solution for the exercise

--- Sandbox/BankAccount.cs
+++ Sandbox/BankAccount.cs
@@ -7,5 +7,48 @@ namespace Sandbox
 {
     public class BankAccount
     {
+        // Two instance fields for representing the name of the account holder,
+        // and the current balance of the account
+        private String accountHolderName;
+        private double balance;
+
+        // Constructor - a name MUST be provided
+        public BankAccount(String name)
+        {
+            accountHolderName = name;
+            balance = 0.0;
+        }
+
+        // Deposit the specified amount into the account
+        public void Deposit(double amount)
+        {
+            balance = balance + amount;
+        }
+
+        // Withdraw the specified amount from the account
+        public void Withdraw(double amount)
+        {
+            balance = balance - amount;
+        }
+
+        // Return the current balance of the account
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        // Return the name of the bank account holder
+        public String GetName()
+        {
+            return accountHolderName;
+        }
+
+        // Interest is here just defined as a simple percentage
+        // of the current balance (not quite realistic...)
+        public void AssignInterest(double interestPercentage)
+        {
+            double interest = balance * (interestPercentage / 100.0);
+            balance = balance + interest;
+        }
     }
 }
--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -8,6 +8,34 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
 
+            // Create a new BankAccount object
+            BankAccount acc = new BankAccount("Anna");
+
+            // What is the initial balance (should be 0)?
+            Console.WriteLine("{0}'s account has a balance of {1}", acc.GetName(), acc.GetBalance());
+
+            acc.Deposit(1000.0);
+            acc.Deposit(1300.0);
+
+            // What is the balance after some deposits (should be 2300)?
+            Console.WriteLine("{0}'s account has a balance of {1}", acc.GetName(), acc.GetBalance());
+
+            acc.Withdraw(2000.0);
+            acc.Withdraw(800.0);
+
+            // What is the balance after some withdrawals (should be -500)?
+            Console.WriteLine("{0}'s account has a balance of {1}", acc.GetName(), acc.GetBalance());
+
+            acc.Deposit(1000.0);
+            acc.Deposit(2800.0);
+
+            // What is the balance after some deposits (should be 3300)?
+            Console.WriteLine("{0}'s account has a balance of {1}", acc.GetName(), acc.GetBalance());
+
+            acc.AssignInterest(6.0);
+
+            // What is the balance after assigning interest (should be 3498)?
+            Console.WriteLine("{0}'s account has a balance of {1}", acc.GetName(), acc.GetBalance());
 
             // The LAST line of code should be ABOVE this line
         }
